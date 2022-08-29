a1ee4d171d79f5bfd0701e958f89b819755a7247
szprx <szprx1910@gmail.com>
2022-08-29 23:01:55 +0200

    fixed error,it was getting null from getAuthentication when were not log in

--- src/main/java/com/dglazewski/shop/gui/view/everyone/ProductsView.java
+++ src/main/java/com/dglazewski/shop/gui/view/everyone/ProductsView.java
@@ -34,65 +34,65 @@ public class ProductsView extends VerticalLayout {
     private final ProductService productService;
     private final CustomerService customerService;
     private final SecurityService securityService;
-    private final Customer customer;
     private final String role = SecurityService.getCurrentUserRole();
 
     public ProductsView(ProductService productService, CustomerService customerService, SecurityService securityService) {
         this.productService = productService;
         this.customerService = customerService;
         this.securityService = securityService;
-        this.customer = customerService.getCustomer(
-                        securityService.getAuthenticatedUser().getUsername())
-                .getEntity();
+
 
         VirtualList<Product> list = new VirtualList<>();
         list.setHeight("820px");
         list.setItems(this.productService.getAllProducts().getEntity());
-        list.setRenderer(productsRenderer);
+        list.setRenderer( new ComponentRenderer<>(product -> {
+            HorizontalLayout cardLayout = new HorizontalLayout();
+            cardLayout.setMargin(true);
+
+            //TODO when amount is equal 0 change name of button to "notify about availablity"
+
+            Button addToShoppingCardButton = new Button("Add to card");
+            addToShoppingCardButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SUCCESS);
+            addToShoppingCardButton.addClickListener(buttonClickEvent -> {
+                if (role.equals("ROLE_ADMIN")) {
+                    Notification.show("You are logged in as ADMIN, action forbidden");
+                }
+                else if (role.equals("ROLE_CUSTOMER")) {
+                    Customer customer = customerService.getCustomer(
+                                    securityService.getAuthenticatedUser().getUsername())
+                            .getEntity();
+
+
+
+                } else {
+
+                }
+
+            });
+            addToShoppingCardButton.setWidth("150px");
+
+            HorizontalLayout activities = new HorizontalLayout(addToShoppingCardButton);
+            activities.setAlignItems(Alignment.CENTER);
+
+            Image image = new Image(product.getImageUrl(), "no image available");
+            image.setWidth(130, Unit.PIXELS);
+            image.setHeight(130, Unit.PIXELS);
+
+            VerticalLayout infoLayout = new VerticalLayout();
+            infoLayout.setSpacing(false);
+            infoLayout.setPadding(false);
+            infoLayout.getElement().appendChild(ElementFactory.createStrong(product.getName()));
+            infoLayout.add(new Div(new Text("Price per kilogram " + product.getPrice() + "$")));
+
+            VerticalLayout contactLayout = new VerticalLayout();
+            contactLayout.setSpacing(false);
+            contactLayout.setPadding(false);
+            contactLayout.add(new Div(new Text("Still available " + product.getAmount() + " kilograms")));
+            infoLayout.add(new Details("More information", contactLayout));
+
+            cardLayout.add(image, infoLayout, activities);
+            return cardLayout;
+        }));
         add(list);
     }
-
-    private ComponentRenderer<Component, Product> productsRenderer = new ComponentRenderer<>(product -> {
-        HorizontalLayout cardLayout = new HorizontalLayout();
-        cardLayout.setMargin(true);
-
-        //TODO when amount is equal 0 change name of button to "notify about availablity"
-
-        Button addToShoppingCardButton = new Button("Add to card");
-        addToShoppingCardButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SUCCESS);
-        addToShoppingCardButton.addClickListener(buttonClickEvent -> {
-            if (role.equals("ROLE_ADMIN")) {
-                Notification.show("You are logged in as ADMIN, action forbidden");
-            }
-            if (role.equals("ROLE_CUSTOMER")) {
-
-            } else {
-
-            }
-
-        });
-        addToShoppingCardButton.setWidth("150px");
-
-        HorizontalLayout activities = new HorizontalLayout(addToShoppingCardButton);
-        activities.setAlignItems(Alignment.CENTER);
-
-        Image image = new Image(product.getImageUrl(), "no image available");
-        image.setWidth(130, Unit.PIXELS);
-        image.setHeight(130, Unit.PIXELS);
-
-        VerticalLayout infoLayout = new VerticalLayout();
-        infoLayout.setSpacing(false);
-        infoLayout.setPadding(false);
-        infoLayout.getElement().appendChild(ElementFactory.createStrong(product.getName()));
-        infoLayout.add(new Div(new Text("Price per kilogram " + product.getPrice() + "$")));
-
-        VerticalLayout contactLayout = new VerticalLayout();
-        contactLayout.setSpacing(false);
-        contactLayout.setPadding(false);
-        contactLayout.add(new Div(new Text("Still available " + product.getAmount() + " kilograms")));
-        infoLayout.add(new Details("More information", contactLayout));
-
-        cardLayout.add(image, infoLayout, activities);
-        return cardLayout;
-    });
 }
